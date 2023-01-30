d721bda05d83248e18d255ed39dcd683ddf4ce23
HENRY JIMENEZ ROSERO <henryjimenez.rosero@gmail.com>
2023-01-30 16:20:00 -0500

    Add files via upload

---
+++ zuul (1)/zuul/src/main/java/com/unir3/zuul/ZuulApplication.java
@@ -0,0 +1,16 @@
+package com.unir3.zuul;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApplication.class, args);
+	}
+
+}
